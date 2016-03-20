c1fff4f005e21039678627a8ab4a2086b70e1d40
Fuad Saud <fuadfsaud@gmail.com>
2016-03-20 18:52:45 -0300

    Add initial app

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <p>HELLO MOTHERFUCKER</p>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
